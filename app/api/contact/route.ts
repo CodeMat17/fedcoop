6db37e53394b0452aa8e7bfa3ae21fd67b4b7f65
CodeMat17 <codemat.biz@gmail.com>
2025-10-15 19:18:36 +0100

    updated

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,8 +1,10 @@
+export const runtime = "nodejs";
+
 import { ContactFormEmail } from "@/emails/contact-form-email";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(request: NextRequest) {
   try {
